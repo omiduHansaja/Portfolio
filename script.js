b5c83004282cc4102de9f58ee49c2946cec388cf
Omidu Hansaja <omiduhansaja@gmail.com>
2026-05-25 11:20:54 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+  reveals.forEach((element) => {
+
+    const windowHeight = window.innerHeight;
+
+    const revealTop = element.getBoundingClientRect().top;
+
+    const revealPoint = 100;
+
+    if(revealTop < windowHeight - revealPoint){
+      element.classList.add("active");
+    }
+
+  });
+
+});
